b13bebead75b5e63385093bc07c47e15323dd4a5
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-04 14:53:59 +0200

    feat: choose display and serif font families

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Amarante, Glass_Antiqua } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const amarante = Amarante({
+  variable: "--font-amarante",
+  weight: "400",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const glassAntiqua = Glass_Antiqua({
+  variable: "--font-glass-antiqua",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -21,7 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="sv"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${amarante.variable} ${glassAntiqua.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <main id="main-content">{children}</main>
