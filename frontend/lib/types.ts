0a22afa189b6921a0ffab56d1cb791f6b3e961cc
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-07 17:06:55 +0200

    feat: define types for api data

---
+++ frontend/lib/types.ts
@@ -0,0 +1,31 @@
+export interface Being {
+  id: number;
+  title: string;
+  element: string;
+}
+
+export interface Municipality {
+  id: string;
+  title: string;
+  type: string;
+}
+
+export interface Observation {
+  id: number;
+  beingId: number;
+  date: string;
+  municipalityId: string;
+  amount: number;
+  habitat: string;
+  behaviour: string;
+  being?: Being;
+  municipality?: Municipality;
+}
+
+export interface ObservationsResponse {
+  observations: Observation[];
+  total: number;
+  limit: number;
+  page: number;
+  pages: number;
+}
