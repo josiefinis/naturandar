3ff28987a859ecf68752575361723a9946cb9658
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-13 12:31:19 +0200

    fix: change skip link text to swedish

--- frontend/components/skip-link.tsx
+++ frontend/components/skip-link.tsx
@@ -4,7 +4,7 @@ export default function SkipLink() {
       href="#main-content"
       className="absolute inset-bs-[calc(-1000px)] focus-visible:inset-bs-0 | inline-full py-4 text-center bg-accent-orange text-white font-display text-fluid-xl"
     >
-      Skip to content
+      Till sidans innehåll
     </a>
   );
 }
