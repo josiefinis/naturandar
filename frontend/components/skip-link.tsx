adda3a1b236b9189ac319cd763f7c3ba1bb4aff3
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-14 10:54:41 +0200

    feat: make skip link fit in magnifiers

--- frontend/components/skip-link.tsx
+++ frontend/components/skip-link.tsx
@@ -2,7 +2,20 @@ export default function SkipLink() {
   return (
     <a
       href="#main-content"
-      className="absolute inset-bs-[calc(-1000px)] focus-visible:inset-bs-0 | inline-full py-4 text-center bg-accent-orange text-white font-display text-fluid-xl"
+      className="
+      absolute 
+      inset-bs-[calc(-1000px)] 
+      focus-visible:inset-bs-0 
+      p-2 m-1 
+      rounded-lg
+      focus-visible:outline-accent-orange
+      focus-visible:outline-solid
+      focus-visible:outline-2
+      bg-accent-orange
+      text-white
+      text-center
+      font-serif
+      text-fluid-md"
     >
       Till sidans innehåll
     </a>
