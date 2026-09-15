098178c5b507a872afcf76bb6536d02b1ff0daef
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-15 11:29:34 +0200

    fix: scale pagination component correctly
    Fixed scaling of SVG icons
    Improved styling

--- frontend/components/pagination.tsx
+++ frontend/components/pagination.tsx
@@ -17,14 +17,14 @@ export default function Pagination({ query, last }: PaginationProps) {
   };
 
   return (
-    <nav className="flex justify-center items-center gap-8 bg-theme-200 text-theme-900 text-fluid-2xl">
+    <nav className="flex justify-center items-center gap-4 bg-theme-200 text-theme-900 text-fluid-2xl my-4 rounded-lg border border-theme-300">
       <Link
         href={{ pathname: "/", query: href(query, previous) }}
         aria-label={`sida ${previous}`}
         scroll={false}
         className="hover:text-accent-orange focus-visible:text-accent-orange"
       >
-        <FleuronLeft height={40} />
+        <FleuronLeft className="size-[1em]" />
       </Link>
       <p className="inline-[6ch] text-center">{`${current} av ${last}`}</p>
       <Link
@@ -33,7 +33,7 @@ export default function Pagination({ query, last }: PaginationProps) {
         aria-label={`sida ${next}`}
         className="hover:text-accent-orange focus-visible:text-accent-orange"
       >
-        <FleuronRight height={40} />
+        <FleuronRight className="size-[1em]" />
       </Link>
     </nav>
   );
