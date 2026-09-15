86d495f30666576118183b6181d2a133d3df2f68
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-15 11:56:58 +0200

    feat: make pagination more screenreader friendly
    - Added screenreader only header for pagination component,
    - Reordered pagination elements to present more intuitively to
    screenreaders.

--- frontend/components/pagination.tsx
+++ frontend/components/pagination.tsx
@@ -18,20 +18,21 @@ export default function Pagination({ query, last }: PaginationProps) {
 
   return (
     <nav className="flex justify-center items-center gap-4 bg-theme-200 text-theme-900 text-fluid-2xl my-4 rounded-lg border border-theme-300">
+      <h2 className="sr-only">Sida</h2>
+      <p className="order-2 inline-[6ch] text-center">{`${current} av ${last}`}</p>
       <Link
         href={{ pathname: "/", query: href(query, previous) }}
         aria-label={`sida ${previous}`}
         scroll={false}
-        className="hover:text-accent-orange focus-visible:text-accent-orange"
+        className="order-1 hover:text-accent-orange focus-visible:text-accent-orange"
       >
         <FleuronLeft className="size-[1em]" />
       </Link>
-      <p className="inline-[6ch] text-center">{`${current} av ${last}`}</p>
       <Link
         href={{ pathname: "/", query: href(query, next) }}
         scroll={false}
         aria-label={`sida ${next}`}
-        className="hover:text-accent-orange focus-visible:text-accent-orange"
+        className="order-3 hover:text-accent-orange focus-visible:text-accent-orange"
       >
         <FleuronRight className="size-[1em]" />
       </Link>
