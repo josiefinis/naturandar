c4883bc244e03edd562a31fa280a84520e00b2f8
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-15 07:53:51 +0200

    feat: clean up footer and associated routes
    Removed non-production and frozen feature pages
    Added about page
    Added references page
    Added external links

---
+++ frontend/app/om/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="font-display text-fluid-2xl text-accent-yellow">Om</h1>
+    </div>
+  );
+}
---
+++ frontend/app/referenser/page.tsx
@@ -0,0 +1,9 @@
+export default function ReferenserPage() {
+  return (
+    <div>
+      <h1 className="font-display text-fluid-2xl text-accent-yellow">
+        Referenser
+      </h1>
+    </div>
+  );
+}
